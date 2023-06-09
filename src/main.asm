2b1343e7b5befbdddd299ea504fc3e32b49e270d
pegvin <75035219+pegvin@users.noreply.github.com>
2023-06-09 13:30:03 +0530

    Hello World Program

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,20 @@
 global _start
 
+section .data
+	msg db "Hello, World", 0x0a ; 0x0a = \n
+	len equ $ - msg ; calculate string length
+
+section .text
+
 _start:
-	mov eax, 1
-	mov ebx, 69
-	int 0x80
+	; Write "msg" to stdout upto "len" bytes
+	mov eax, 4   ; sys_write
+	mov ebx, 1   ; file handler/descriptor for stdout
+	mov ecx, msg ; pointer to the string
+	mov edx, len ; number of bytes to write
+	int 0x80     ; interrupt and call the sys_write (4) call
+
+	; Exit status
+	mov eax, 1   ; sys_exit
+	mov ebx, 0   ; exit status code
+	int 0x80     ; interrupt to call the sys_exit (1) call
